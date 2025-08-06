e97cbe2275660cd5207a72a05bb802d50896aad7
aryan <aryadav@ttu.edu>
2025-08-05 20:09:35 -0500

    jwt error1

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,7 @@ const createSendToken = (user, statusCode, res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: "None",
   };
 
   if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
